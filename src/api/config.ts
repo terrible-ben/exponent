547d6f6eef91705495081d4416a3b3b75c67712b
Kairos0ne <kairos0ne@icloud.com>
2024-11-23 14:39:05 +0000

    remove node_modules with ignore

--- src/api/config.ts
+++ src/api/config.ts
@@ -5,22 +5,22 @@ export class ConfigAPI {
   constructor(private client: APIClient) {}
 
   getConfig(configId: string) {
-    return this.client.request('GET', `/configs/${configId}`);
+    return this.client.request('GET', `/api/v1/configs/${configId}`);
   }
 
   listConfigs() {
-    return this.client.request('GET', '/configs');
+    return this.client.request('GET', '/api/v1/configs');
   }
 
   createConfig(configData: any) {
-    return this.client.request('POST', '/configs', configData)
+    return this.client.request('POST', '/api/v1/configs', configData)
   }
 
   updateConfig(configData: any, configId: string ) {
-    return this.client.request('PUT', `/configs/${configId}`, configData)
+    return this.client.request('PUT', `/api/v1/configs/${configId}`, configData)
   }
 
   deleteConfig(configId: string) {
-    return this.client.request('DELETE', `/configs/${configId}`)
+    return this.client.request('DELETE', `/api/v1/configs/${configId}`)
   }
 }
